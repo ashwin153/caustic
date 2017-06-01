db083a11b53e52dc9363c2ef5e456b954d140c66
Ashwin Madavan <ashwin.madavan@gmail.com>
2017-06-01 14:11:33 -0700

    Fixed flapping concurrency unit test

--- schema-runtime/src/test/scala/schema/runtime/local/LockFreeDatabaseTest.scala
+++ schema-runtime/src/test/scala/schema/runtime/local/LockFreeDatabaseTest.scala
@@ -1,8 +1,11 @@
 package schema.runtime.local
 
+import org.junit.runner.RunWith
 import org.scalatest.Outcome
+import org.scalatest.junit.JUnitRunner
 import schema.runtime.DatabaseTest
 
+@RunWith(classOf[JUnitRunner])
 class LockFreeDatabaseTest extends DatabaseTest {
 
   override def withFixture(test: OneArgTest): Outcome =

e99221b6cd5442a62289bf2e97bc8ff89ac54e54
Ashwin Madavan <ashwin.madavan@gmail.com>
2017-06-01 13:26:52 -0700

    Updated benchmarks

--- schema-benchmark/src/main/scala/schema/benchmark/DatabaseBenchmark.scala
+++ schema-benchmark/src/main/scala/schema/benchmark/DatabaseBenchmark.scala
@@ -8,16 +8,17 @@ import scala.concurrent.duration.Duration
 
 object DatabaseBenchmark extends Bench.OfflineReport {
 
-  val database: Database = local.SynchronizedDatabase("x" -> "0.0", "y" -> "1.0")
+  // Fake database to "mock" database performance.
+  val database: Database = new FakeDatabase
 
   // Benchmark transactions containing sequential reads.
-  val sequential: Gen[Transaction] = Gen
+  val transactions: Gen[Transaction] = Gen
     .exponential("size")(2, 1 << 10, 2)
     .map(size => Seq.fill(size)(read(literal("x"))).reduce((a, b) => cons(a, b)))
 
   performance of "Database" in {
     measure method "execute" in {
-      using(sequential) curve "Transaction Size" in { txn =>
+      using(transactions) curve "Transaction Size" in { txn =>
         Await.result(database.execute(txn), Duration.Inf)
       }
     }

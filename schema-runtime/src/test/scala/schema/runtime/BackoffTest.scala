5e3586078032ec2523dd3faac93c91f6617c4ed5
Ashwin Madavan <ashwin.madavan@gmail.com>
2017-06-04 03:33:10 -0700

    Fixed BackoffTest.scala

--- schema-runtime/src/test/scala/schema/runtime/BackoffTest.scala
+++ schema-runtime/src/test/scala/schema/runtime/BackoffTest.scala
@@ -2,8 +2,7 @@ package schema.runtime
 
 import Backoff._
 import org.junit.runner.RunWith
-import org.mockito.ArgumentMatchers._
-import org.mockito.Mockito.{verify, when}
+import org.mockito.Mockito._
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.{FunSuite, Matchers}
@@ -20,7 +19,7 @@ class BackoffTest extends FunSuite
 
   test("Backoff properly retries failures.") {
     val fake = mock[Database]
-    when(fake.execute(any())(any()))
+    when(fake.execute(literal(""))(global))
       .thenReturn(Future.failed(new Exception("Retryable.")))
       .thenReturn(Future("Success"))
       .thenReturn(Future.failed(FatalException("Non-Retryable")))
@@ -38,7 +37,7 @@ class BackoffTest extends FunSuite
       Await.result(retry(Seq(1 second))(fake.execute(literal(""))).failed, 500 millis)
     }
 
-    verify(fake)
+    verify(fake, times(4)).execute(literal(""))(global)
   }
 
 }

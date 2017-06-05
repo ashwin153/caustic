b0b7d1a549c179997b7b0813357e9f56fa0a6601
Ashwin Madavan <ashwin.madavan@gmail.com>
2017-06-04 20:36:20 -0700

    Automated Release Process
    Added release.sh to perform releases. Closes #14.

---
+++ release.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+read -p "Artifact version (defaults to incrementing patch version): " version
+read -r -p "Are you sure? [y/N] " response
+
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]] ; then
+  # Publish Build Artifacts
+  if [ -z "version" ] ; then
+    ./pants publish.jar --publish-jar-no-dryrun \
+      schema-runtime/src/main/scala \
+      schema-mysql/src/main/scala \
+      schema-postgresql/src/main/scala
+  else
+    ./pants publish.jar --publish-jar-no-dryrun \
+      --publish-jar-override=com.madavan#schema-runtime_2.12=$version \
+      --publish-jar-override=com.madavan#schema-mysql_2.12=$version \
+      --publish-jar-override=com.madavan#schema-postgresql_2.12=$version \
+      schema-runtime/src/main/scala \
+      schema-mysql/src/main/scala \
+      schema-postgresql/src/main/scala
+  fi
+
+  # Promote to Maven Central
+  /usr/bin/open -a "/Applications/Google Chrome.app" \
+    'http://www.pantsbuild.org/release_jvm.html#promoting-to-maven-central'
+  /usr/bin/open -a "/Applications/Google Chrome.app" \
+    'https://oss.sonatype.org/#stagingRepositories'
+fi

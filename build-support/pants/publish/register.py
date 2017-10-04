b2cee45f08e20230214c43f91f19dd83b94685f8
Ashwin Madavan <ashwin.madavan@gmail.com>
2017-10-04 13:24:18 -0500

    Reseting

--- build-support/pants/publish/register.py
+++ build-support/pants/publish/register.py
@@ -1,40 +1,43 @@
 # pants/publish/register.py
 from pants.build_graph.build_file_aliases import BuildFileAliases
 from pants.backend.jvm.repository import Repository
-from pants.backend.jvm.ossrh_publication_metadata import (Developer, License,
-                                                          OSSRHPublicationMetadata, Scm)
+from pants.backend.jvm.ossrh_publication_metadata import (
+    Developer, License, OSSRHPublicationMetadata, Scm
+)
+
 import os
 
 repository = Repository(
-    name = 'public',
-    url = 'https://oss.sonatype.org/#stagingRepositories',
-    push_db_basedir = os.path.join('build-support', 'ivy', 'pushdb')
+    name='public',
+    url='https://oss.sonatype.org/#stagingRepositories',
+    push_db_basedir=os.path.join('build-support', 'ivy', 'pushdb')
 )
 
 metadata = OSSRHPublicationMetadata(
-    description = 'A library for expressing and executing transactions.',
-    url = 'https://github.com/ashwin153/schema',
-    licenses = [
+    description='A library for expressing and executing transactions.',
+    url='https://github.com/ashwin153/caustic',
+    licenses=[
         License(
-            name = 'Apache License, Version 2.0',
-            url = 'http://www.apache.org/licenses/LICENSE-2.0'
+            name='Apache License, Version 2.0',
+            url='http://www.apache.org/licenses/LICENSE-2.0'
         )
     ],
-    developers = [
+    developers=[
         Developer(
-            name = 'Ashwin Madavan',
-            email = 'ashwin.madavan@gmail.com',
-            url = 'http://madavan.me'
+            name='Ashwin Madavan',
+            email='ashwin.madavan@gmail.com',
+            url='https://madavan.me'
         )
     ],
-    scm = Scm.github(
-        user = 'ashwin153',
-        repo = 'schema'
+    scm=Scm.github(
+        user='ashwin153',
+        repo='caustic'
     )
 )
 
+
 def build_file_aliases():
-    return BuildFileAliases(objects = {
+    return BuildFileAliases(objects={
         'public': repository,
         'describe': metadata,
     })

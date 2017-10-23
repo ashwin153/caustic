a9e18942bbd46152ffcba1f198b0f656a53d838b
Ashwin Madavan <ashwin.madavan@gmail.com>
2017-10-22 21:05:46 -0500

    Extracted Main

--- caustic-runtime/src/main/scala/caustic/runtime/Server.scala
+++ caustic-runtime/src/main/scala/caustic/runtime/Server.scala
@@ -6,8 +6,7 @@ import org.apache.thrift.server.TNonblockingServer
 import org.apache.thrift.transport.TNonblockingServerSocket
 import pureconfig._
 
-import java.io.{Closeable, File}
-import java.net.{URL, URLClassLoader}
+import java.io.Closeable
 
 /**
  * An executable, runtime instance.
@@ -81,35 +80,6 @@ object Server {
     val underlying = config.caches.foldRight(Database.forName(config.database))(Cache.forName)
     val registry = Option(config.discoverable) collect { case true => Registry() }
     Server(underlying, config.port, registry)
-  }
-
-  /**
-   * An entry-point that bootstraps and serves a Server. Server configurations are loaded from the
-   * classpath, but may be overridden by providing a path to a configuration file path or by
-   * explicitly setting the value of system properties.
-   */
-  object Main extends App {
-
-    if (args.length > 1) {
-      println("Usage: ./pants run caustic-runtime/src/main/scala:server [config] -- -Dprop=value")
-      System.exit(1)
-    }
-
-    // Add configuration file to classpath. https://stackoverflow.com/a/7884406/1447029
-    if (args.nonEmpty) {
-      val uri = new File(args(0)).toURI
-      val classLoader = ClassLoader.getSystemClassLoader.asInstanceOf[URLClassLoader]
-      val method = classOf[URLClassLoader].getDeclaredMethod("addURL", classOf[URL])
-      method.setAccessible(true)
-      method.invoke(classLoader, Array(uri.toURL))
-    }
-
-    // Asynchronously bootstrap server, and tear it down on shutdown.
-    val server = Server()
-    sys.addShutdownHook {
-      this.server.close()
-    }
-
   }
 
 }

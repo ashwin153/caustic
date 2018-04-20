2bb86b286620dc0a84731650880b58d6cd1c7700
Ashwin Madavan <ashwin.madavan@gmail.com>
2018-04-19 22:39:10 -0500

    Added Pointer jsonformat

--- caustic-library/src/main/scala/caustic/library/external/Pointer.scala
+++ caustic-library/src/main/scala/caustic/library/external/Pointer.scala
@@ -1,8 +1,19 @@
 package caustic.library.external
 
+import spray.json._
+
 /**
  *
  * @param key
  * @tparam T
  */
 case class Pointer[T](key: java.lang.String)
+
+object Pointer {
+
+  implicit def format[T]: JsonFormat[Pointer[T]] = new RootJsonFormat[Pointer[T]] {
+    override def read(x: JsValue): Pointer[T] = Pointer(x.convertTo[String])
+    override def write(x: Pointer[T]): JsValue = x.key.toJson
+  }
+
+}
